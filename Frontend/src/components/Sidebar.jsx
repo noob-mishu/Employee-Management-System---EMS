d46852387a746b2d6d01d5e78ece1248d04d9eba
noob-mishu <mishubarua320@gmail.com>
2026-06-14 23:14:12 +0600

    updating sidebar

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from 'react-router-dom'
 import {dummyProfileData} from '../assets/assets'
+import { MenuIcon } from 'lucide-react'
 
 const Sidebar = () => {
 
@@ -22,8 +23,21 @@ const Sidebar = () => {
 
 
   return (
-    <div>Sidebar</div>
-  )
+    <>
+    //mobile button
+
+    <button onClick={()=>setmobileOpen(true)} className='lg:hidden fixed top-4 left-4 z-50 p-2 bg-slate-900 text-white rounded-lg shadow-lg border border-white/10' >
+      <MenuIcon size={20} />
+    </button>
+    {/*mobile overlay*/ }
+    {mobileOpen && 
+      <div className='lg:hidden fixed inset-0 z-40 bg-black/50' blackdrop-blur-sm onClick={()=>setmobileOpen(false)}>
+
+      </div>
+    }
+    
+    </>
+    )
 }
 
 export default Sidebar
