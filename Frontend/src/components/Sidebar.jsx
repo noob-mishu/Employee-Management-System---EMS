f75efe6a7a303939cdda74f51afd2aded7aa2f27
noob-mishu <mishubarua320@gmail.com>
2026-06-16 12:02:18 +0600

    Sidebar updated

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { dummyProfileData } from "../assets/assets";
 import { MenuIcon, UserIcon, XIcon } from "lucide-react";
@@ -23,7 +23,7 @@ const Sidebar = () => {
       <div className="text-2xl font-bold mb-6 text-center border-b border-white/10 pb-4">
         <div className="flex items-center justify-center">
           <div className="flex items-center gap-3">
-            <UserIcon classname="text-white size-7" />
+            <UserIcon className="text-white size-7" />
             <div>
               <p className="text-lg font-semibold text-white tracking-wide">
                 Employee MS
@@ -34,7 +34,11 @@ const Sidebar = () => {
             </div>
           </div>
 
-          <button onClick={() => setmobileOpen(false)} className="lg:hidden text-slate-400 hover:text-white p-1" >
+          {/*Mobile Close Button*/}
+          <button
+            onClick={() => setmobileOpen(false)}
+            className="lg:hidden text-slate-400 hover:text-white p-1"
+          >
             <XIcon size={20} />
           </button>
         </div>
@@ -51,7 +55,7 @@ const Sidebar = () => {
 
   return (
     <>
-      //mobile button
+      {/*Mobile Button*/}
       <button
         onClick={() => setmobileOpen(true)}
         className="lg:hidden fixed top-4 left-4 z-50 p-2 bg-slate-900 text-white rounded-lg shadow-lg border border-white/10"
@@ -61,14 +65,15 @@ const Sidebar = () => {
       {/*mobile overlay*/}
       {mobileOpen && (
         <div
-          className="lg:hidden fixed inset-0 z-40 bg-black/50"
-          blackdrop-blur-sm
+          className="lg:hidden fixed inset-0 z-40 bg-black/50 blackdrop-blur-sm"
           onClick={() => setmobileOpen(false)}
         ></div>
       )}
       {/*sidebar-desktop*/}
       <aside
-        className={`fixed top-0 left-0 h-full w-64 bg-slate-800 text-white p-4 z-50 transform ${mobileOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:flex-shrink-0 $ {mobileOpen ? "translate-x-0" : "-translate-x-full"}`}
+        className={`fixed top-0 left-0 h-full w-64 bg-slate-800 text-white p-4 z-50 transform ${
+          mobileOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out lg:translate-x-0 lg:static lg:flex-shrink-0`}
       >
         {sidebarContent}
       </aside>
