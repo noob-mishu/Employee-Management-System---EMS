9c5cf4cf4f3c26ac507d02874c9cf074da123688
noob-mishu <mishubarua320@gmail.com>
2026-06-20 23:49:17 +0600

    Dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,35 @@
+import React, { useEffect, useState } from "react";
+import { dummyEmployeeDashboardData } from "../assets/assets";
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Dashboard
+  useEffect(() => {
+    setData(dummyEmployeeDashboardData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="text-center text-gray-500">Loading dashboard...</div>
+    );
+  }
+
+  if (!data) {
+    return (
+      <div className="text-center text-red-500">
+        Failed to load dashboard data.
+      </div>
+    );
+  }
+
+  if (data.role === "ADMIN") {
+    return <div>admin dashboard</div>;
+  }
+
+  return <div>employee dashboard</div>;
+};
+
+export default Dashboard;
